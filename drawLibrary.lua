56edace5fb91271b8276990dbbe11b8bbba712f6
samuelrblx <111674708+samuelrblx@users.noreply.github.com>
2023-04-13 20:12:56 +0100

    Update drawLibrary.lua

--- drawLibrary.lua
+++ drawLibrary.lua
@@ -6,7 +6,7 @@ getgenv().tweenService = game:GetService"TweenService"
 
 -- 35, 129, 236
     
-local uicolor = Color3.fromRGB(73, 128, 255)
+local uicolor = Color3.fromRGB(48, 122, 228)
 
 if getgenv().library then
     getgenv().library:Unload()
